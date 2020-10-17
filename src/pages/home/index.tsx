9677645111a0de28f8aab80679173593a776d3f2
Werawit Sirakunwat <werawit_boat@windowslive.com>
2020-10-17 14:01:44 +0700

    Add: basic route setup

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { HomeContainer } from './styled';
+
+const Home = (): React.ReactElement => {
+    return (
+        <HomeContainer>
+            <h1>Hello from Home.</h1>
+        </HomeContainer>
+    );
+}
+
+export default Home;
