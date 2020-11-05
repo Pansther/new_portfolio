fad7535a16c65e392ebe07668dbd6654564e0fc2
Werawit Sirakunwat <werawit_boat@windowslive.com>
2020-11-05 15:07:52 +0700

    Add: project skeleton when loading

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';  
+import { Skeleton } from 'antd';
 
 import { projectType } from '../../types/project';
 
@@ -12,42 +13,91 @@ interface IProps {
     groupName: String,
     loading: boolean
 }
+
+/// get project status and return tailwindcss color class.
+function getColorClass(isComplete: Boolean) {
+    return isComplete ? 'bg-green-400' : 'bg-red-400';
+};
+  
+const Project = (project: projectType): React.ReactElement => { 
+    return (
+        <a key={project.id} href={`${project.path}`} target='_blank' rel="noopener noreferrer">
+            <ProjectCard
+                title={`${project.name}`} 
+                hoverable
+                // cover={<ProjectCover alt='cover' src={`${project.img}`} />}
+                cover={<div className='project-test-cover h-56 w-full bg-blue-200'></div>}
+            >
+                <div className='project-status-box flex items-center'>
+                    <div className={`project-status h-3 w-3 rounded-full p-0 my-3 ${getColorClass(project.complete)}`}></div>
+                    <p className='p-0 ml-3 flex-baseline'>{`${project.complete ? 'Completed' : 'In Progress'}`}</p>
+                </div>
+                <p className='break-all font-light'>{project.content}</p>
+            </ProjectCard>
+        </a>
+    );
+}
+
+const ProjectSkeleton = (): React.ReactElement => {
+
+    function createMockupData (minDataNumber: number, maxDataNumber: number) {
+        let project: projectType[] = [];
+
+        let number = Math.floor(Math.random() * (maxDataNumber - minDataNumber)) + minDataNumber;
+
+        for (let i = 0; i < number; i++) {
+            project.push({
+                id: 1,
+                name: 'test',
+                img: '',
+                content: '',
+                path: '',
+                complete: Math.random() >= 0.5
+            });
+        }
+
+        return project;
+    }
+
+    return (
+        <>
+            {
+                createMockupData(4, 8).map(project => (
+                    <a key={project.id} href={`${project.path}`} target='_blank' rel="noopener noreferrer">
+                        <ProjectCard
+                            title={<div className='h-5 w-24 bg-gray-200 animate-pulse'></div>}
+                            hoverable
+                            // cover={<ProjectCover alt='cover' src={`${project.img}`} />}
+                            cover={<div className='h-56 w-full bg-gray-200 animate-pulse'></div>}
+                        > 
+                            <Skeleton paragraph={{ rows: 4 }} className='animate-pulse'></Skeleton>
+                        </ProjectCard>
+                    </a>
+                ))
+            }
+        </>
+    );
+}
  
 const ProjectGroup = (props: IProps): React.ReactElement => {
 
     // console.log(project_data);
     // console.log(loading);
-
-    /// get project status and return tailwindcss color class.
-    const getColorClass = (isComplete: Boolean) => {
-        return isComplete ? 'bg-green-400' : 'bg-red-400';
-    };
-  
-    return (
-        <ProjectBox className='project-box'> 
+ 
+    return (  
+        <ProjectBox className='project-box'>
             <VeryLargeText className='group-name'><b>{props.groupName}</b></VeryLargeText>
             <div className='project-list'>
                 {
+                    props.loading ?  
+                        <ProjectSkeleton />
+                    :
                     props.project_data?.map(project => (
-                        <a key={project.id} href={`${project.path}`} target='_blank' rel="noopener noreferrer">
-                            <ProjectCard
-                                title={`${project.name}`} 
-                                loading={props.loading}
-                                hoverable
-                                // cover={<ProjectCover alt='cover' src={`${project.img}`} />}
-                                cover={<div className='project-test-cover h-56 w-full bg-blue-200'></div>}
-                            > 
-                                <div className='project-status-box overflow-auto flex items-center'>
-                                    <div className={`project-status h-3 w-3 rounded-full p-0 my-3 ${getColorClass(project.complete)}`}></div>
-                                    <p className='p-0 ml-3 flex-baseline'>{`${project.complete ? 'Completed' : 'In Progress'}`}</p>
-                                    </div>
-                                <p className='break-all font-light'>{project.content}</p>
-                            </ProjectCard>
-                        </a>
-                    ))
-                } 
-            </div>  
-        </ProjectBox>
+                        <Project { ...project } />
+                    ))   
+                }
+            </div>
+        </ProjectBox> 
     );
 }
 
