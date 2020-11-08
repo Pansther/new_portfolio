722e6b0513466014ded943d2128ce0e1487c296d
Werawit Sirakunwat <werawit_boat@windowslive.com>
2020-11-08 19:48:04 +0700

    Add: add useTabPane

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';  
+import { Tabs } from 'antd';
 
 import { HomeContainer } from './styled';
 
@@ -7,10 +8,12 @@ import HomeCover from './cover';
 import ProjectGroup from '../../components/project';
 import ContactGroup from '../../components/contact';
 
+const { TabPane } = Tabs;
+
 const Home = (): React.ReactElement => {  
 
     const [projectData, setProject] = React.useState([]);
-    const [loading, setLoading] = React.useState(true);
+    const [loading, setLoading] = React.useState(true); 
 
     React.useEffect(() => { 
         axios.get(`${process.env.REACT_APP_LOCAL}/project/`)
@@ -29,14 +32,29 @@ const Home = (): React.ReactElement => {
 
     // console.log(projectData);
 
+    function useTabPane (tabName: string, tabKey: number, Component: React.ReactElement) {
+        return (
+            <TabPane tab={tabName} key={tabKey}>
+                { Component }
+            </TabPane>
+        );
+    }
+
+    const ProjectTab = useTabPane('My Projects', 1, <ProjectGroup groupName='My Projects' project_data={projectData} loading={loading} />);
+    const AboutTab = useTabPane('About Me', 2, <></>);
+    
     return (
         <HomeContainer>
             <HomeCover />
             <ContactGroup />
-            <ProjectGroup groupName='My Projects' project_data={projectData} loading={loading} />  
-            {/* <ProjectGroup groupName='Participant Activity' project_data={[]} loading={loading} />   */}
+
+            <Tabs defaultActiveKey="1" type="card" centered>
+                { ProjectTab }
+                { AboutTab }
+            </Tabs>
+
         </HomeContainer>
-    );
+    ); 
 }
 
 export default Home;
