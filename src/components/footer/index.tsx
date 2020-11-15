ca98b325444ff6d8107e99adb18726414ee84fc9
Werawit Sirakunwat <werawit_boat@windowslive.com>
2020-11-15 17:37:00 +0700

    Update: update footer height

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterContainer } from './styled';
 
 const Footer = (): React.ReactElement => {
     return (    
-        <FooterContainer className='h-56 mt-10 bg-blue-200'>
+        <FooterContainer className='h-20 mt-10 bg-gray-800'>
             <h1>Hello from Footer.</h1> 
         </FooterContainer>
     );
